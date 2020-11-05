ffbffcb4942a3b2b6fd10a0aa073b5c9253ccaa1
Alex V <2038611+lexffe@users.noreply.github.com>
2020-11-05 13:51:02 +0000

    boiler gen

---
+++ desktop/src/uk/ac/york/cs/thegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.ac.york.cs.thegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.ac.york.cs.thegame.TheGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheGame(), config);
+	}
+}
