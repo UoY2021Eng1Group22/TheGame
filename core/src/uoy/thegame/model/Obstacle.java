dd2563c08101bf876a4a8b20992c54c5b0ccb7ef
TomClarkYorkie <72925924+TomClarkYorkie@users.noreply.github.com>
2020-11-16 12:48:27 +0000

    Play screen changes and Obstacle now extends actor + many changes inside obstacle to accomodate this

--- core/src/uoy/thegame/model/Obstacle.java
+++ core/src/uoy/thegame/model/Obstacle.java
@@ -2,42 +2,54 @@ package uoy.thegame.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 
-public class Obstacle {
-    private final int[] position;
-    private final Texture obsImg;
+public class Obstacle extends Actor {
+    private final Sprite obsImg;
     int capableDamage;
     String[][] possibleSprites = {{"skin/obs1.png", "skin/obs2.png"}, {"skin/obs1.png", "skin/obs2.png"}, {"skin/obs3.png", "skin/obs4.png"}, {"skin/obs5.png"}};
 
-//    public Obstacle() {
-//
-//    }
 
     public Obstacle(int levelNum) {
-        position = new int[2];
+
+        var ranSprite = (int) (Math.random() * (possibleSprites[levelNum - 1].length));
+        obsImg = new Sprite( new Texture(possibleSprites[levelNum - 1][ranSprite]));       // chooses a sprite from the appropriate set
 
         this.setPosition(
                 (int) (Math.random() * Gdx.graphics.getWidth()), // Generates random x and y coordinates
                 (int) (Math.random() * (Gdx.graphics.getHeight() - 120) + 120)
         );
 
-        var ranSprite = (int) (Math.random() * (possibleSprites[levelNum - 1].length));
-        obsImg = new Texture(possibleSprites[levelNum - 1][ranSprite]);       // chooses a sprite from the appropriate set
+        setBounds(obsImg.getX(),obsImg.getY(),obsImg.getWidth(), obsImg.getHeight());
     }
 
-    public Texture getObsImg() {
+    public Sprite getObsImg() {
         return obsImg;
     }
 
     public int[] getPosition() {
-        return position;
+        int[] pos = {(int)obsImg.getX(),(int)obsImg.getY()};
+        return pos;
     }
 
     public void setPosition(int x, int y) {
-        this.position[0] = x;
-        this.position[1] = y;
+        obsImg.setPosition(x,y);
     }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha){
+        obsImg.draw(batch);
+    }
+
+    @Override
+    public void act(float delta){
+        super.act(delta);
+    }
+
+
 }
 
 
--- core/src/uoy/thegame/view/PlayScreen.java
+++ core/src/uoy/thegame/view/PlayScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -41,17 +42,7 @@ public class PlayScreen implements Screen {
         var currentStageObstacles = levelInfo.getCurrentStageObstacles();
 
         for (Obstacle currentObstacle : currentStageObstacles) {              // mostly copied from above setup, non functional
-
-            var obsPos = currentObstacle.getPosition();
-            var obsImg = currentObstacle.getObsImg();
-
-            var obstRegion = new TextureRegion(obsImg);
-            obstRegion.setRegion(obsPos[0], obsPos[1], obsImg.getWidth(), obsImg.getHeight());
-
-            var obsImage = new Image(obstRegion);
-            obsImage.setPosition(obsPos[0], obsPos[1]);
-
-            stage.addActor(obsImage);
+            stage.addActor(currentObstacle);
         }
 
 
